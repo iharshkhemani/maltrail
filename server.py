73b6e2c9de96987dc64bd220795d4d076771942c
iharshkhemani <86166599+iharshkhemani@users.noreply.github.com>
2023-05-28 16:05:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-
-"""
-Copyright (c) 2014-2023 Maltrail developers (https://github.com/stamparm/maltrail/)
-See the file 'LICENSE' for copying permission
-"""
-
 from __future__ import print_function  # Requires: Python >= 2.6
 
 import sys
